d2336f15e2c0d6748d1a434f9b09ece90700f61c
Andrew Haw <github@andrewh.ca>
2013-07-02 20:55:38 -0700

    Installing print css stylesheet

--- inc/deck-css.php
+++ inc/deck-css.php
@@ -1,7 +1,8 @@
 
 		<!-- start deck.js css // -->
-		<link rel="stylesheet" href="/slidin/css/deck.core.css" media="screen">
-		<link rel="stylesheet" href="/slidin/css/deck.status.css" media="screen">
-		<link rel="stylesheet" href="/slidin/css/deck.transition.fade.css" media="screen">
+		<link rel="stylesheet" href="<?php echo $dir; ?>slidin/css/deck.core.css" media="screen">
+		<link rel="stylesheet" href="<?php echo $dir; ?>slidin/css/deck.status.css" media="screen">
+		<link rel="stylesheet" href="<?php echo $dir; ?>slidin/css/deck.transition.fade.css" media="screen">
+		<link rel="stylesheet" href="<?php echo $dir; ?>slidin/css/print.css" media="screen">
 		<!-- // end deck.js css -->
 		
